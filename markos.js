3360acf9f3925a35898551393b0b96f8ab1013c2
Mohireza <mohireza50@gmail.com>
2022-04-22 11:17:12 +0430

    V1.0

--- markos.js
+++ markos.js
@@ -1,7 +1,8 @@
 'use strict';
-const endglishDatabase = [];
-const persianDatabase = [];
+let endglishDatabase = [];
+let persianDatabase = [];
 const wordAdder = string => {
+  let wordCounter = 0;
   const words = string
     .toLowerCase()
     .split(
@@ -15,6 +16,7 @@ const wordAdder = string => {
       !persianDatabase.includes(word)
     ) {
       persianDatabase.push(word);
+      wordCounter++;
     } else if (
       /[A-Za-z0-9\u00a9\u00ae\u2000-\u3300\ud83c\ud000-\udfff\ud83d\ud000-\udfff\ud83e\ud000-\udfff]+/gi.test(
         word
@@ -22,28 +24,41 @@ const wordAdder = string => {
       !endglishDatabase.includes(word)
     ) {
       endglishDatabase.push(word);
+      wordCounter++;
     }
+  }
+  if (wordCounter > 1) {
+    document.querySelector(
+      '.succ'
+    ).textContent = `${wordCounter} new words were added successfully!`;
+  } else if (wordCounter === 1) {
+    document.querySelector(
+      '.succ'
+    ).textContent = `One new word was added successfully!`;
+  } else {
+    document.querySelector(
+      '.succ'
+    ).textContent = `No new words were added to the database!`;
   }
 };
 const wordMaker = () => {
   // The functions selects one of the two arrays based on a random number (the longer array has more chance)
   let selectedArray = [];
-  if (
+  const arrChance =
     Math.random() * (persianDatabase.length / endglishDatabase.length + 1) <
-    persianDatabase.length / endglishDatabase.length
-  ) {
-    selectedArray = [...persianDatabase];
-  } else if (
-    Math.random() * (persianDatabase.length / endglishDatabase.length + 1) >
-    persianDatabase.length / endglishDatabase.length
-  ) {
-    selectedArray = [...endglishDatabase];
-  } else if (endglishDatabase.length === 0 && persianDatabase.length === 0) {
+    persianDatabase.length / endglishDatabase.length;
+  if (endglishDatabase.length === 0 && persianDatabase.length === 0) {
     return 'ERR_EMPTY_DATABASES';
-  } else if (persianDatabase.length === 0) {
+  } else if (endglishDatabase.length < 5 && persianDatabase.length < 5) {
+    return 'Please add more than 5 words';
+  } else if (persianDatabase.length < 5) {
     selectedArray = [...endglishDatabase];
-  } else if (endglishDatabase.length === 0) {
+  } else if (endglishDatabase.length < 5) {
     selectedArray = [...persianDatabase];
+  } else if (arrChance) {
+    selectedArray = [...persianDatabase];
+  } else if (!arrChance) {
+    selectedArray = [...endglishDatabase];
   }
   if (Math.random() < 0.8) {
     let numberOfWords = Math.floor(Math.random() * 3) + 1;
@@ -89,12 +104,22 @@ const wordMaker = () => {
 };
 document.querySelector('.submit').addEventListener('click', () => {
   wordAdder(document.querySelector('#input').value);
-  console.log(persianDatabase);
-  if (document.querySelector('#input').value)
-    document.querySelector('.succ').textContent = 'Successfully added!';
+  if (document.querySelector('#input').value);
   else if (!document.querySelector('#input').value)
     document.querySelector('.succ').textContent = 'Please enter a string!';
+  document.querySelector('#input').value = '';
 });
 document.querySelector('.generate').addEventListener('click', () => {
   document.querySelector('.random').textContent = wordMaker();
+});
+document.querySelector('.clear').addEventListener('click', () => {
+  if (persianDatabase.length > 0 || endglishDatabase.length > 0) {
+    persianDatabase = [];
+    endglishDatabase = [];
+    document.querySelector('.random').textContent =
+      'Database successfully cleared!';
+  } else {
+    document.querySelector('.random').textContent =
+      'Database is already empty!';
+  }
 });
